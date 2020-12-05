7803fa11ddde5c27ee3efb345a8bcdfec701b026
Hoyt Reid <hoytsreid@gmail.com>
2020-12-05 17:08:24 +0000

    Day four part two solve

--- src/PassportProcessing.php
+++ src/PassportProcessing.php
@@ -25,7 +25,7 @@ class PassportProcessing {
     return $parsedInput;
   }
 
-  public function countValidPassports(array $passports) {
+  public function countValidPassports(array $passports, bool $validateData = false) {
     $validPassportCount = 0;
     $passportKeys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
@@ -36,16 +36,87 @@ class PassportProcessing {
         }
       }
 
-      $validPassportCount += 1;
+      if($validateData) {
+        if ($this->validateData($passport)) {
+          $validPassportCount += 1;
+        }
+      } else {
+        $validPassportCount += 1;
+      }
     }
 
     return $validPassportCount;
   }
 
+  public function validateData(array $passport) {
+    return
+      $this->validateByr($passport['byr']) &&
+      $this->validateIyr($passport['iyr']) &&
+      $this->validateEyr($passport['eyr']) &&
+      $this->validateHgt($passport['hgt']) &&
+      $this->validateHcl($passport['hcl']) &&
+      $this->validateEcl($passport['ecl']) &&
+      $this->validatePid($passport['pid']);
+  }
+
+  public function validateByr(string $byr) {
+    $parsedByr = intval($byr);
+
+    return strlen($byr) == 4 && $parsedByr >= 1920 && $parsedByr <= 2002;
+  }
+
+  public function validateIyr(string $iyr) {
+    $parsedIyr = intval($iyr);
+
+    return strlen($iyr) == 4 && $parsedIyr >= 2010 && $parsedIyr <= 2020;
+  }
+
+  public function validateEyr(string $eyr) {
+    $parsedEyr = intval($eyr);
+
+    return strlen($eyr) == 4 && $parsedEyr >= 2020 && $parsedEyr <= 2030;
+  }
+
+  public function validateHgt(string $hgt) {
+    $unit = substr($hgt, -2);
+
+    if(!($unit == 'in' || $unit == 'cm')) {
+      return false;
+    }
+
+    $measure = intval(substr($hgt, 0, strlen($hgt) - 2));
+
+    if ($unit == 'in') {
+      return $measure >= 59 && $measure <= 76;
+    } else {
+      return $measure >= 150 && $measure <= 193;
+    }
+  }
+
+  public function validateHcl(string $hcl) {
+    if(substr($hcl, 0, 1) != '#' || strlen($hcl) != 7) {
+      return false;
+    }
+
+    return (bool) preg_match('/([0-9]|[a-f]){6}/', substr($hcl, 1));
+  }
+
+  public function validateEcl(string $ecl) {
+    $validEclList = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+
+    return in_array($ecl, $validEclList);
+  }
+
+  public function validatePid(string $pid) {
+    return strlen($pid) == 9 && ctype_digit($pid);
+  }
+
   public function generateAnswer() {
     $input = $this->readFile();
     $validPassportCount = $this->countValidPassports($input);
+    $validPassportCountWithData = $this->countValidPassports($input, true);
 
     print "Day 4 Part 1 Answer: " . $validPassportCount . PHP_EOL;
+    print "Day 4 Part 2 Answer: " . $validPassportCountWithData . PHP_EOL;
   }
 }
--- tests/PassportProcessingTest.php
+++ tests/PassportProcessingTest.php
@@ -44,10 +44,103 @@ class PassportProcessingTest extends TestCase {
     $processedPassports = [];
 
     foreach ($passports as $passport) {
-      $processedPassports[] = $parsedInput = $this->passportProcessing->parseInput($passport);
+      $processedPassports[] = $this->passportProcessing->parseInput($passport);
     }
 
     $validPassportCount = $this->passportProcessing->countValidPassports($processedPassports);
     $this->assertEquals(2, $validPassportCount);
+  }
+
+  public function testValidateByr() {
+    $validByr = '2002';
+    $invalidByr = '2003';
+
+    $this->assertTrue($this->passportProcessing->validateByr($validByr));
+    $this->assertFalse($this->passportProcessing->validateByr($invalidByr));
+  }
+
+  public function testValidateHgt() {
+    $validInHgt = '60in';
+    $validCmHgt = '190cm';
+    $invalidInHgt = '190in';
+    $invalidHgt = '190';
+
+    $this->assertTrue($this->passportProcessing->validateHgt($validInHgt));
+    $this->assertTrue($this->passportProcessing->validateHgt($validCmHgt));
+    $this->assertFalse($this->passportProcessing->validateHgt($invalidInHgt));
+    $this->assertFalse($this->passportProcessing->validateHgt($invalidHgt));
+  }
+
+  public function testValidateHcl() {
+    $validHcl = '#123abc';
+    $invalidHclLetter = '#123abz';
+    $invalidHclFormat = '123abc';
+
+    $this->assertTrue($this->passportProcessing->validateHcl($validHcl));
+    $this->assertFalse($this->passportProcessing->validateHcl($invalidHclLetter));
+    $this->assertFalse($this->passportProcessing->validateHcl($invalidHclFormat));
+  }
+
+  public function testValidateEcl() {
+    $validEcl = 'brn';
+    $invalidEcl = 'wat';
+
+    $this->assertTrue($this->passportProcessing->validateEcl($validEcl));
+    $this->assertFalse($this->passportProcessing->validateEcl($invalidEcl));
+  }
+
+  public function testValidatePid() {
+    $validPid = '000000001';
+    $invalidPid = '0123456789';
+
+    $this->assertTrue($this->passportProcessing->validatePid($validPid));
+    $this->assertFalse($this->passportProcessing->validatePid($invalidPid));
+  }
+
+  public function testValidatePasswordData() {
+    $invalidData = [
+      'eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926',
+
+      'iyr:2019
+      hcl:#602927 eyr:1967 hgt:170cm
+      ecl:grn pid:012533040 byr:1946',
+
+      'hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277',
+
+      'hgt:59cm ecl:zzz
+      eyr:2038 hcl:74454a iyr:2023
+      pid:3556412378 byr:2007'
+    ];
+
+    $validData = [
+      'pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      hcl:#623a2f',
+      'eyr:2029 ecl:blu cid:129 byr:1989
+      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm',
+      'hcl:#888785
+      hgt:164cm byr:2001 iyr:2015 cid:88
+      pid:545766238 ecl:hzl
+      eyr:2022',
+      'iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719'
+    ];
+
+    $invalidPassports = [];
+    $validPassports = [];
+
+    foreach ($invalidData as $passport) {
+      $invalidPassports[] = $this->passportProcessing->parseInput($passport);
+    }
+
+    foreach ($validData as $passport) {
+      $validPassports[] = $this->passportProcessing->parseInput($passport);
+    }
+
+    $countValidInvalidPassports = $this->passportProcessing->countValidPassports($invalidPassports, true);
+    $countValidValidPassports = $this->passportProcessing->countValidPassports($validPassports, true);
+
+    $this->assertEquals(0, $countValidInvalidPassports);
+    $this->assertEquals(4, $countValidValidPassports);
   }
 }
